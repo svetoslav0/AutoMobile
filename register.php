429bd27e56ea851b551a9c8e6b633db2ada26cb1
loradoncheva <loradoncheva@abv.bg>
2018-12-10 18:02:20 +0200

    Created 'views' folder and separated register.php file

--- register.php
+++ register.php
@@ -5,43 +5,8 @@
         echo "<div style='color: red; font-weight: bold'>" . $_SESSION['error'] . "</div>";
         unset($_SESSION['error']);
     }
-?>
-<h2>Register:</h2>
-    <hr>
-<form method="post">
-    <table>
-        <tr>
-            <td>Username: <span style="color: red">*</span></td>
-            <td><input type="text" name="username" /></td>
-        </tr>
-        <tr>
-            <td>Password: <span style="color: red">*</span></td>
-            <td><input type="password" name="password" /></td>
-        </tr>
-        <tr>
-            <td>Confirm password: <span style="color: red">*</span></td>
-            <td><input type="password" name="confirm_password" /></td>
-        </tr>
-        <tr>
-            <td>Email: <span style="color: red">*</span></td>
-            <td><input type="text" name="email" /></td>
-        </tr>
-        <tr>
-            <td>First name:</td>
-            <td><input type="text" name="first_name" /></td>
-        </tr>
-        <tr>
-            <td>Last name:</td>
-            <td><input type="text" name="last_name" /></td>
-        </tr>
-        <tr>
-            <td><input type="submit" value="Register" name="submit" /></td>
-        </tr>
-    </table>
-</form>
 
-<?php
-
+include_once ("views/register.view.php");
 include_once ("db_connect.php");
 
 if (isset($_POST['submit'])){
---
+++ views/register.view.php
@@ -0,0 +1,34 @@
+<h2>Register:</h2>
+    <hr>
+<form method="post">
+    <table>
+        <tr>
+            <td>Username: <span style="color: red">*</span></td>
+            <td><input type="text" name="username" /></td>
+        </tr>
+        <tr>
+            <td>Password: <span style="color: red">*</span></td>
+            <td><input type="password" name="password" /></td>
+        </tr>
+        <tr>
+            <td>Confirm password: <span style="color: red">*</span></td>
+            <td><input type="password" name="confirm_password" /></td>
+        </tr>
+        <tr>
+            <td>Email: <span style="color: red">*</span></td>
+            <td><input type="text" name="email" /></td>
+        </tr>
+        <tr>
+            <td>First name:</td>
+            <td><input type="text" name="first_name" /></td>
+        </tr>
+        <tr>
+            <td>Last name:</td>
+            <td><input type="text" name="last_name" /></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Register" name="submit" /></td>
+        </tr>
+    </table>
+</form>
+
