499700128801d25a3eeda434c79d56fd03cb1756
svetoslav0 <sssvetoslavvv@gmail.com>
2018-12-12 22:27:29 +0200

    Created rowCount and insert methods in Repository class, script for adding new advertisement, updated add-car.view.php, created uploads folder to store the uploaded pictures

--- add-car.php
+++ add-car.php
@@ -1,6 +1,50 @@
 <?php
 
 session_start();
-
+if (!isset($_SESSION['id'])){
+    header('Location: login.php');
+}
+include_once ('views/header.view.php');
 include_once ('views/add-car.view.php');
+
+if (isset($_POST['add_car_button'])){
+    include_once ('upload.php');
+    $brand = $_POST['brand'];
+    $model = $_POST['model'];
+    $user_id = $_SESSION['id'];
+    $state = $_POST['state'];
+    $release_year = $_POST['release_year'];
+    $price = $_POST['price'];
+    $power = $_POST['power'];
+    $volume = $_POST['engine_volume'];
+    $category = $_POST['categories'];
+    $eurostandart = $_POST['eurostandarts'];
+    $engine_type = $_POST['engines'];
+    $gears = $_POST['gears'];
+    $mileage = $_POST['mileage'];
+    $color = $_POST['colors'];
+    $town = $_POST['towns'];
+
+    $extras = [];
+    $extrasCount = Repository::getRowsCount($db, 'extras');
+    for($i = 0; $i < $extrasCount; $i++){
+        if (isset($_POST[$i])){
+            $extras[] = $i;
+        }
+    }
+
+    $extras = implode(',', $extras);
+
+    Repository::insert($db, 'ads', [
+        $user_id, $brand, $model, $state, $release_year, $price,
+        $power, $volume, $category, $eurostandart, $engine_type,
+        $gears, $mileage, $color, $town, $extras, $_FILES['fileToUpload']['name']
+    ]);
+}
+
+
+
+
+
+
 
--- test.php
+++ test.php
@@ -1,23 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Svetoslav
- * Date: 26-Nov-18
- * Time: 8:35 AM
- */
-
-$password  = "Some_Password";
-
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-echo $hashed_password;
-
-if (password_verify("Some_Password",
-    '$2y$10$rZervNEbrjRg5bl3jHnBpeJcpbytOajaz.bCdO.cEoQeRz4zyspD6')){
-    echo 'Correct password!';
-}else{
-    echo 'Incorrect password<br />';
-    echo 'One more change';
-    echo 'One more change to do, i hope it is the last...';
-}
-
+<form action="">
+    <input type="submit">
+</form>
